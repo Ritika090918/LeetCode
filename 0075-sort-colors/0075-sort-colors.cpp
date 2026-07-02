00449661f714a5ba553abd2927268f5311755a9e
Ritika kumari <ritika99103@gmail.com>
2026-07-02 18:42:07 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.66%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int no0=0;
+        int no1=0;
+        int no2=0;
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                no0++;
+            } else if(nums[i]==1){
+                no1++;
+            } else{
+                no2++;
+            }
+        };
+        for(int i=0; i<n; i++){
+        if(i<no0){
+            nums[i]=0;
+        } else if(i<(no0+no1)){
+            nums[i]=1;
+        } else {
+            nums[i]=2;
+        }
+    }
+    
+    return;
+    }
+};
